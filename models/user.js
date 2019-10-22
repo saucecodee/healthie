5242eebf685e523084e1a4dac2290f32876cf5a6
Gloria <gloriauraphael@gmail.com>
2019-10-22 16:07:08 +0100

    added user model

--- models/user.js
+++ models/user.js
@@ -1,40 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-     name: {
-          type: String,
-          required: true
-     },
-     phone: {
-          type: String,
-          required: true
-     },
-     email: {
-          type: String,
-          required: true,
-          unique: true,
-     },
-     password: {
-          type: String,
-          required: true
-     },
-     address: {
-          type: String,
-          required: true
-     },
-     imgUrl: {
-          type: String,
-          default: "/uploads/defaultImg.png"
-     },
-     isActive: {
-          type: Boolean,
-          default: true
-     },
-     isBlocked: {
-          type: Boolean,
-          default: false
-     }
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+    default: "/uploads/defaultImg.png",
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: date,
+    default: new Date(),
+  },
+  appointments: [{id: String}],
 });
 
-module.exports = mongoose.model('Users', UserSchema);
+module.exports = mongoose.model("Users", UserSchema);
