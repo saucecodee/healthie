e4c0f557f78bc37cb994f6083d81a60a32359c49
Gloria <gloriauraphael@gmail.com>
2019-10-23 18:51:45 +0100

    Added User setup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,68 @@
-const { getUsers, editUser } = require('../services/userService')
-const { response } = require('../config/messages')
+const {
+  signupUser,
+  signinUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  editUser,
+} = require("../services/userServices");
+
+const { response } = require("../config/messages");
 
 class UserContoller {
-    getUsers(req, res) {
-        getUsers(req.body)
-            .then(result => {
-                res.status(result.status).send(result)
-            }).catch(error => {
-                res.status(result.status).send(response(404, "Something went wrong", error))
-            })
+  async signupUser(req, res, next) {
+    try {
+      await signupUser(req.body);
+      res.status(201).send(response("Account created"));
+    } catch (error) {
+      next(error);
     }
+  }
 
-    editUser(req, res) {
-        editUser(req.body)
-            .then(result => {
-                res.status(result.status).send(result)
-            }).catch(error => {
-                res.status(result.status).send(response(404, "Something went wrong", error))
-            })
+  async signinUser(req, res, next) {
+    try {
+      await signinUser(req.body);
+      res.status(200).send(response("User signed in"));
+    } catch (error) {
+      next(error);
     }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+      users = await getUsers();
+      res.status(200).send(response("All users", users));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getUser(req, res, next) {
+    try {
+      user = await getUser(req.params.userId);
+      res.status(200).send(response("User detail", user));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async editUser(req, res, next) {
+    try {
+      user = await editUser(req.params.userId, data);
+      res.status(200).send(response("Profile edited"));
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteUser(req, res, next) {
+    try {
+      user = await deleteUser(req.params.userId);
+      res.status(200).send(response("User deleted"));
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
-module.exports = new UserContoller()
+module.exports = new UserContoller();
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
 
-module.exports = function (app) {
-    app.get('/test', (req, res)=> {
-        res.send("yehh!")
-    })
-    app.use('/user', userRoutes())
-    return router;
-}
+module.exports = function(app) {
+  app.use("/user", userRoutes());
+  return router;
+};
