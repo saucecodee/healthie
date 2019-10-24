4bac11bfa9924549c283f53ecba59860ec40fd99
saucecodee <ashawinta@gmail.com>
2019-10-24 15:27:35 +0100

    added created at to models

--- models/appointment.js
+++ models/appointment.js
@@ -1,38 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const AppointmentSchema = new Schema({
-     doctor: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true
-     },
-     user: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true
-     },
-     dateCreated: {
-          type: Date,
-          default: Date.now(),
-     },
-     duration: {
-          type: Number,
-          required: true
-     },
-     location: {
-          type: String,
-          required: true
-     },
-     status: {
-          type: String,
-          enum: ['pending', 'approved', 'accepted', 'canceled'],
-          default: 'pending',
-          lowercase: true,
-          trim: true,
-     }
+const AppointmentSchema = new Schema(
+     {
+          doctor: {
+               type: mongoose.Schema.Types.ObjectId,
+               required: true
+          },
+          user: {
+               type: mongoose.Schema.Types.ObjectId,
+               required: true
+          },
+          duration: {
+               type: Number,
+               required: true
+          },
+          location: {
+               type: String,
+               required: true
+          },
+          status: {
+               type: String,
+               enum: ['pending', 'approved', 'accepted', 'canceled'],
+               default: 'pending',
+               lowercase: true,
+               trim: true,
+          }
 
-},
-{
-     timestamps: true
-});
+     },
+     {
+          timestamps: true
+     });
 
 module.exports = mongoose.model('Appointment', AppointmentSchema);
--- models/doctors.js
+++ models/doctors.js
@@ -1,64 +1,68 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const DoctorSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const DoctorSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    specialty: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+      default: '/uploads/defaultImg.png',
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    dateCreated: {
+      type: Date,
+      default: Date.now(),
+    },
+    licenceID: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+      default: null,
+    },
+    appointments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Appointment',
+      }
+    ]
   },
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  specialty: {
-    type: String,
-    required: true,
-  },
-  imgUrl: {
-    type: String,
-    default: '/uploads/defaultImg.png',
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-  isBlocked: {
-    type: Boolean,
-    default: false,
-  },
-  dateCreated: {
-    type: Date,
-    default: Date.now(),
-  },
-  licenceID: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  bio: {
-    type:String,
-    default: null,
-  },
-  appointments: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Appointment',
-    }
-  ]
-});
+  {
+    timestamps: true
+  });
 
 module.exports = mongoose.model('Doctors', DoctorSchema);
